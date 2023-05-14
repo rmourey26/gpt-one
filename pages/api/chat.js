15617b4aa1b0ad4909b2d547ad23c36ed34e14f4
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-14 12:28:05 -0400

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,10 +1,10 @@
 // Make sure to add OPENAI_API_KEY as a secret
 
 import { Configuration, OpenAIApi } from "openai";
-import config from "src/aws-exports";
+import 'awsconfig' from "./aws-exports";
 
 // Amplify SSR configuration needs to be done within each API route
-Amplify.configure({ ...config, ssr: true });
+Amplify.configure({ ...awsconfig, ssr: true });
 
 
 
