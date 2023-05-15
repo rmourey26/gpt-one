b0c310dc0fba6c81cc8a489e875e415ba030ecdb
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-15 14:17:44 -0400

    ..

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -33,7 +33,7 @@ export default async function(req, res) {
   const requestMethod = req.method;
   const completion = await openai.createChatCompletion({
     // You need early access to GPT-4, otherwise use "gpt-3.5-turbo"
-    model: "gpt-4",
+    model: "gpt-3.5-turbo",
     messages: [{ "role": "system", "content": "You help customers make more informed decisions pertaining to their health care and guide them toward the best health plan available at healthcare.gov based upon their age, marital status, number of children, budget, coverage requirements, preferred medical providers, if anyone has any ongoing diseases or disabilities requiring atypical medical care. Additionally, you never ask the customer to upload or provide any photos as our website has no means of doing so at this time. Also, do not mention that you are a bot." }].concat(req.body.messages),
   });
   switch (requestMethod) {
