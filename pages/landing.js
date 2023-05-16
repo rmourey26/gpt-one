e4578d0d84b448aedc734de4c4b40a496e5bd2c4
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-16 13:06:48 -0400

    .

--- pages/landing.js
+++ pages/landing.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import NestedGrid from 'components/nestedgrid'
 
 const navigation = [
   { name: 'Alt Employee Health Benefits', href: '#' },
@@ -103,7 +104,7 @@ export default function Example() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#60bbf0] to-[#60afce] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -127,7 +128,7 @@ export default function Example() {
             <p className="mt-6 text-lg leading-8 text-gray-600">
               Solutions for a healthier you - AntHealth.ai
             </p>
-            
+            <img className="w-auto" src="https://quantumone.b-cdn.net/AntHealth_Logos/Workplace.svg?color=sky&shade=600" height={350} width={350}/>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               
               <a
@@ -142,6 +143,7 @@ export default function Example() {
             </div>
           </div>
         </div>
+        <NestedGrid />
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
@@ -154,7 +156,9 @@ export default function Example() {
             }}
           />
         </div>
+        
       </div>
+      
     </div>
   )
 }
