0a90dbf270373296e30152bb98c5d23f2ab772ce
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-16 11:34:41 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
-  { name: 'Health Plans', href: '#', current: false },
-  { name: 'AI', href: '#', current: false },
+  { name: 'Alternative Benefits Mgmt', href: '#', current: false },
+  { name: 'Health Benefits Mgmt', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
 ]
 
@@ -36,7 +36,7 @@ export default function Home() {
  
   const [loading, setLoading] = useState(false);
   const [messages, setMessages] = useState([
-    {role: "assistant", content: "Hi and welcome to AntHealth's AI Healthplan Design App!"}
+    {role: "assistant", content: "Welcome to AntHealth.ai!"}
     ]);
   
       const messageListRef = useRef(null);
@@ -143,12 +143,12 @@ export default function Home() {
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
-                    src="https://quantumone.b-cdn.net/AntHealth_Logos/ah-nb.svg?color=indigo&shade=500"
+                    src="https://quantumone.b-cdn.net/AntHealth_Logos/ah-nb.svg?color=sky&shade=500"
                     alt="Your Company"
                   />
                   <img
                     className="hidden h-8 w-auto lg:block"
-                    src="https://quantumone.b-cdn.net/AntHealth_Logos/ah-nb.svg??color=indigo&shade=500"
+                    src="https://quantumone.b-cdn.net/AntHealth_Logos/ah-nb.svg??color=sky&shade=500"
                     alt="Your Company"
                   />
                 </div>
@@ -261,6 +261,34 @@ export default function Home() {
     </Disclosure>
       
       <main className={styles.main}>
+      <div className="items-center px-4 flex justify-center mb-10" >
+            <div className="relative mr-3">
+                <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
+                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </div>
+                <input
+                    type="text"
+                    className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
+                    placeholder="Search chronic diseases..."
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+            </div>
+</div>
+<div className="items-center px-4 flex justify-center mb-10" >
+            <div className="relative mr-3">
+                <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
+                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </div>
+                <input
+                    type="text"
+                    className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
+                    placeholder="Search health plans..."
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+            </div>
+</div>
         <div className={styles.cloud}>
           <div ref={messageListRef} className={styles.messagelist}>
             {messages.map((message, index) => {
@@ -288,20 +316,7 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.center}>
-        <div className="items-center px-4 flex justify-center mb-10" >
-            <div className="relative mr-3">
-                <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
-                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-                </div>
-                <input
-                    type="text"
-                    className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
-                    placeholder="Search Here..."
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                />
-            </div>
-</div>
+        
           <div className={styles.cloudform}>
             <form onSubmit={handleSubmit}>
               <textarea
@@ -314,7 +329,7 @@ export default function Home() {
                 type="text"
                 id="userInput"
                 name="userInput"
-                placeholder={loading ? "Waiting for response..." : "Tell me your healthcare scenario..."}
+                placeholder={loading ? "Waiting for response..." : "Ask me about ABPM or your chronic condition..."}
                 value={userInput}
                 onChange={e => setUserInput(e.target.value)}
                 className={styles.textarea}
