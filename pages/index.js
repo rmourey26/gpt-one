7e12d34d17d2810c5c24a8d0afc9f8713c6af6d0
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-15 21:39:45 -0400

    tailwind search bar

--- pages/index.js
+++ pages/index.js
@@ -147,7 +147,20 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.center}>
-
+        <div className="items-center px-4 flex justify-center" >
+            <div className="relative mr-3">
+                <div className="absolute top-3 left-3 items-center" ref={clickPoint}>
+                    <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </div>
+                <input
+                    type="text"
+                    className="block p-2 pl-10 w-70 text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:pl-3"
+                    placeholder="Search Here..."
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+            </div>
+</div>
           <div className={styles.cloudform}>
             <form onSubmit={handleSubmit}>
               <textarea
