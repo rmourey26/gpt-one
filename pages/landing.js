643e8b88686ade6879b430ac61c41e0f15823274
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-22 03:25:36 -0400

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -114,7 +114,7 @@ export default function Example() {
       </header>
       <section class="bg-white dark:bg-gray-900">
     <div class="space-y-28 grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div class="mr-auto place-self-center lg:col-span-7">
+        <div class="mr-auto place-self-center sm:col-span-4 lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none sm:text-4xl md:text-5xl xl:text-6xl dark:text-white">AI Powered Benefits</h1>
             <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Empowering intelligent health plan and employer benefit design.</p>
             <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
