ee29113ebc0ab2c7c5bc7983bfad7e0b8b2f648e
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-22 16:20:46 -0400

    Update profileform.js

--- pages/profileform.js
+++ pages/profileform.js
@@ -133,6 +133,33 @@ export default function Example() {
                 />
               </div>
             </div>
+<div className="sm:col-span-2">
+
+              <label htmlFor="suffix" className="block text-sm font-medium leading-6 text-gray-900">
+
+                Suffix
+
+              </label>
+
+              <div className="mt-2">
+
+                <input
+
+                  type="text"
+
+                  name="suffix"
+
+                  id="suffix"
+
+                  autoComplete="suffix"
+
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+
+                />
+
+              </div>
+
+            </div>
 
             <div className="sm:col-span-4">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
