3a5bf953bf7bb0efa78e11187537e2648bd1ffce
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-22 16:22:01 -0400

    Update profileform.js

--- pages/profileform.js
+++ pages/profileform.js
@@ -17,7 +17,7 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 export default function Example() {
   return (
     <form>
-      <div className="space-y-12">
+      <div className="space-y-12 m-2">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
