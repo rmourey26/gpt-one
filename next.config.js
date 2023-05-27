255cff5dbd7e2cd9bfea7c7ed0e9c0c59aef45a3
rmourey_jr@quantumone.network <rmourey_jr@quantumone.network>
2023-05-27 10:47:31 -0400

    .

--- next.config.js
+++ next.config.js
@@ -4,5 +4,13 @@ const nextConfig = {
   images : {
     domains : ['quantumone.b-cdn.net', 'unpkg.com'],
   },
-}
+  async rewrites() {
+    return [
+      {
+        source: "/api/aca",
+        destination: "https://marketplace.api.healthcare.gov/api/v1",
+      },
+    ];
+  },
+};
 module.exports = nextConfig
