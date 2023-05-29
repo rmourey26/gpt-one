d6fb30d9911fbe0558d7e39f2be8f7b394ad9b1c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-29 01:29:59 -0400

    Update landing.js

--- pages/landing.js
+++ pages/landing.js
@@ -112,6 +112,7 @@ onBlur={handleBlur}
 
                   </div>
 </div>
+</div>
                 </div>
               </div>
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
