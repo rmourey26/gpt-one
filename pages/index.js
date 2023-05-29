3163d968c528596a43bed629e0855bb3bea0d515
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-28 22:38:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -223,7 +223,7 @@ const toggle = (index) => {
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
+            <div className="relative flex h-16 items-stretch justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -235,7 +235,7 @@ const toggle = (index) => {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="flex flex-1 items-stretch justify-between sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <img
                     className="block h-8 w-auto lg:hidden"
@@ -249,6 +249,19 @@ const toggle = (index) => {
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
+                  <input
+
+                        type="text"
+
+                        className="block p-2 pl-10 w-80 text-gray-900 bg-gray-50 rounded-lg border border-sky-500 focus:pl-3"
+
+                        placeholder="Search plans, chronic diseases, etc...."
+
+                        onFocus={handleFocus}
+
+                        onBlur={handleBlur}
+
+                      />
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
                       <a
