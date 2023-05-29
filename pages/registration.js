cf8f41b67f3d5e3a0694be077638a33c9d491958
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-05-28 21:44:46 -0400

    Update registration.js

--- pages/registration.js
+++ pages/registration.js
@@ -24,7 +24,7 @@ export default function Example() {
 
     
     <form>
-      <div className="space-y-12 lg:mx-36 my-16 sm:mx-8 my-4">
+      <div className="space-y-12 lg:mx-36 my-16 sm:mx-10 my-5">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold text-center text-gray-900">AntHealth Registration/ Sign Up Process</h2>
           <p className="mt-1 text-sm text-center text-gray-600">
