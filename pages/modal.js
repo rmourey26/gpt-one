098338a3378821f2553eb3340070bf41fd23e622
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-01 03:23:32 -0400

    Update modal.js

--- pages/modal.js
+++ pages/modal.js
@@ -4,7 +4,7 @@ import react from 'react';
 
 export default function Modal() {
   return (
-    <>
+   
     <section class="bg-gray-50 dark:bg-gray-900">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"> AntHealth
@@ -15,10 +15,13 @@ export default function Modal() {
           
       <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
-              </h1>
+              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Sign in to your account</h1>
+
+              
+                  
               <form class="space-y-4 md:space-y-6" action="#">
+
+              
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                       <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" onChange={(e) => setPost(e.target.value))}></input>
@@ -47,7 +50,7 @@ export default function Modal() {
       </div>
   </div>
 </section>
-</>
+
     )
 };
 
