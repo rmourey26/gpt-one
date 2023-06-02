9bafd3f26c14b0e0867bdd009cac5c3f315c0699
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-02 18:06:26 -0400

    ..

--- pages/api/form.js
+++ pages/api/form.js
@@ -8,12 +8,12 @@ export default function handler(req, res) {
    
     // Guard clause checks for first and last name,
     // and returns early if they are not found
-    if (!body.first || !body.last) {
+    if (!body.firstName || !body.lastName) {
       // Sends a HTTP bad request error code
       return res.status(400).json({ data: 'First or last name not found' });
     }
    
     // Found the name.
     // Sends a HTTP success code
-    res.status(200).json({ data: `${body.first} ${body.last}` });
+    res.status(200).json({ data: `${body.firstName} ${body.lastName}` });
   }
--- pages/registration.js
+++ pages/registration.js
@@ -25,12 +25,12 @@ export default function Example() {
 
 <Navbar />
 <div className="space-y-12 lg:m-36 md:m-20 sm:m-10">
-    <form>
+    <form action="/api/form" method="post">
       <div className="space-y-12 lg:mx-36 my-16 sm:mx-10 my-5">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold text-center text-gray-900">AntHealth Registration/ Sign Up Process</h2>
           <p className="mt-1 text-sm text-center text-gray-600">
-           Who are you insuring?
+           To test this API, complete the form and save. If successful, you will see the name you entered on the next page. If you receive an error, please let me know.
           </p>
 
          
@@ -42,30 +42,30 @@ export default function Example() {
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
-              <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
                 First name
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
+                  name="firstName"
+                  id="firstName"
+                  autoComplete="givenName"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
                 Last name
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
+                  name="lastName"
+                  id="lastName"
+                  autoComplete="familyName"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -229,7 +229,7 @@ export default function Example() {
                     <label htmlFor="products" className="font-medium text-gray-900">
                       Products 
                     </label>
-                    <p className="text-gray-500">Get notified when we launch new AntHealth products andbservices.</p>
+                    <p className="text-gray-500">Get notified when we launch new AntHealth products and services.</p>
                   </div>
                 </div>
                 <div className="relative flex gap-x-3">
