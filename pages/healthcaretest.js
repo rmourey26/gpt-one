a805b83b546b71210e90426e941ddd0908ab55f1
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-04 16:33:05 -0400

    .

--- pages/healthcaretest.js
+++ pages/healthcaretest.js
@@ -1,21 +1,63 @@
 // pages/healthcare.js
 import { useState, useEffect } from 'react';
 import NoSSR from 'components/nossr';
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 
 export default function Healthcare({ initialData }) {
   const [data, setData] = useState(initialData);
+
+  const rows = [transformData(initialData)];
+
+  let columns = [];
+
+  if (rows.length > 0) {
+    columns = Object.keys(rows[0]).map((key) => ({
+      field: key,
+      headerName: key.charAt(0).toUpperCase() + key.slice(1),
+      width: 100
+    }));
+  }
 
   // Render logic can go here. For example, you might want to map over the data array
   return (
     <div>
       <NoSSR>
-    
+    <div className="h-max m-10 w-100">
       {/* Render the data here */}
+      <h1 className='text-center mb-10'>Flattened ACA Response in Table</h1>
+      <DataGrid className='overflow-x:scroll h-100'
+      rows={rows} 
+      columns={columns}
+      getRowId={(row) => row.plans_0_id + row.plans_0_name }
+      height={500}
+    
+       />
+      </div>
+      <h1 className='m-10 text-center'>Original JSON Object from ACA</h1>
+      <div className='m-10'>
       {JSON.stringify(data, null, 2)}
+      </div>
       </NoSSR>
     </div>
   );
 }
+function transformData(data) {   
+  const flattenedData = {};   
+  function flatten(data, prefix = '') {
+         for (let key in data) {
+                 const value = data[key];             
+                 if (typeof value === 'object' && value !== null) {
+                     flatten(value, `${prefix}${key}_`);
+                     } else {
+                       flattenedData[`${prefix}${key}`] = value;       
+                      }
+                    }  
+                   }   
+                   flatten(data);   
+                   
+                   return flattenedData; 
+                  }
 
 export async function getServerSideProps(context) {
   const data = {
