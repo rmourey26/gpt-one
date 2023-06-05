e6d5279af9b3449eccc18494ca4577c95cd6c25b
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-04 20:44:36 -0400

    Update healthsummary.jsx

--- pages/healthsummary.jsx
+++ pages/healthsummary.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import openai from 'openai';
-import NoSSR from 'components/NoSSR;
+import NoSSR from 'components/NoSSR';
 
 // initialize OpenAI with your API key
 openai.setApiKey(process.env.OPENAI_API_KEY);
