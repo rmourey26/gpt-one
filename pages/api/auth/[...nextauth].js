b79746941feb1cbb7d6280d35c7c8715356f5399
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-06 20:32:06 -0400

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import CredentialsProvider from "next-auth/providers/credentials"
-
+import OktaProvider from "next-auth/providers/okta"
 
 
 export const authOptions = {
