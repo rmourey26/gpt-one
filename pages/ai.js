27469fdddc59247863b90c8dbd1e69000892ca2b
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-11 17:53:48 -0400

    card

--- pages/ai.js
+++ pages/ai.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import Navbar from 'components/navbar';
 import Footer from 'components/footer';
+import DOMPurify from 'dompurify';
 
 
 export default function AI() {
@@ -47,6 +48,7 @@ export default function AI() {
       
   
   <form onSubmit={onSubmit}>
+  <div class="mb-6">
   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
   <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     name="gender"
@@ -56,6 +58,7 @@ export default function AI() {
     <option value="man">Male</option>
     <option value="woman">Female</option>
   </select>
+  </div>
 
   <div class="mb-6">
   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Age</label>
@@ -83,6 +86,7 @@ export default function AI() {
     onChange={(e) => setBudgetMin(Number.parseInt(e.target.value))}
   />
 </div>
+<div class="mb-6">
   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Budget Maximum</label>
   <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     type="number"
@@ -92,9 +96,9 @@ export default function AI() {
     value={budgetMax}
     onChange={(e) => setBudgetMax(Number.parseInt(e.target.value))}
   />
-
+</div>
 <div class="mb-6">
-  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Chronic</label>
+  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Chronic Conditions</label>
   <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     type="text"
     name="chronic"
@@ -126,15 +130,19 @@ export default function AI() {
   <button type="submit" class="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800">Submit</button>
 </form>
 {loading && (
-  <div>
+  <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
     <h3>Looking for the best health plans</h3>
     <img src="" className={styles.loading} />
   </div>
 )}
+{result && (
+  <div className="mt-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 <div 
   className={styles.acaanswer}
   dangerouslySetInnerHTML={{ __html: result }}
 />
+</div>
+)}
 </div>
 </div>
 <Footer />
