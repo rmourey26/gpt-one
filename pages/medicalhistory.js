3c7a64bb5a3ba59d0dd057926bfcf6b0375e976c
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-13 16:05:02 -0400

    ..

--- pages/medicalhistory.js
+++ pages/medicalhistory.js
@@ -3,12 +3,28 @@ import Navbar from 'components/navbar'
 import Head from 'next/head'
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 import dynamic from 'next/dynamic'
+import { useSession, signIn, signOut } from "next-auth/react"
+
+import { useRouter } from 'next/router';
+
+import NoSSR from "components/nossr"; 
+
+import styles from '../styles/Home.module.css';
+
+import LogAuth from "components/login-btn";
+
 
 const MedSurveyComponent = dynamic(() => import("../components/MedicalSurveyComp"),{
   ssr:false,
 })
 
 const MedicalHistory = () => {
+
+  const { data: session } = useSession()
+
+  const router = useRouter();
+
+  if (session) {
     return (
     
       <>
@@ -17,6 +33,29 @@ const MedicalHistory = () => {
       <Footer />
       </>
     )
-};
+}
+
+return (
+  <div className={styles.center}>
+  <div className={styles.container}>
+
+<div className="mt-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+     <h1 className="title">Welcome to AntHealth</h1>
+
+    <div className={styles.content}>
+
+        <h2> To continue, please sign in</h2>
+
+    <LogAuth />
+
+    </div>
+ </div>
+  </div>
+  </div>
+
+)
+
+}
 
 export default MedicalHistory;
