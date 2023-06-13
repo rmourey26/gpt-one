49ec751566a3d0a937c6c6252eb5a85c45ead47d
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-13 15:49:59 -0400

    auth

--- pages/questionaire.js
+++ pages/questionaire.js
@@ -16,7 +16,15 @@ import Footer from 'components/footer'
 import Navbar from 'components/navbar'
 import Head from 'next/head'
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import { useSession, signIn, signOut } from "next-auth/react"
+
+import { useRouter } from 'next/router';
+
+import styles from '../styles/Home.module.css';
+
+import LogAuth from "components/login-btn";
+
 
 const SurveyComponent = dynamic(() => import("../components/SurveyComp"),{
   ssr:false,
@@ -25,6 +33,11 @@ const SurveyComponent = dynamic(() => import("../components/SurveyComp"),{
 
 
 const Questionaire = () => {
+  const { data: session } = useSession()
+
+  const router = useRouter();
+
+  if (session) {
   return (
   
     <>
@@ -58,4 +71,28 @@ const Questionaire = () => {
     
   )
 }
+
+return (
+  <div className={styles.center}>
+  <div className={styles.container}>
+
+<div className="mt-5 block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+
+     <h1 className="title">Welcome to AntHealth</h1>
+
+    <div className={styles.content}>
+
+        <h2> To continue, please sign in</h2>
+
+    <LogAuth />
+
+    </div>
+ </div>
+  </div>
+  </div>
+
+)
+
+}
+
 export default Questionaire
