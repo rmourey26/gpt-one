9210a55732671fb0b93bd5fd9c2333fd9e60aecd
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-18 17:07:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,9 +44,9 @@ export default function Home() {
               <p class="max-w-2xl text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Empowering intelligent health plan and employer benefit design.</p>
               <div className="items-center px-4 flex justify-center" >
                   <div className="relative mr-3">
-                
-                   <button href="/ai" class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-
+                <Link href="/antchat" passHref>
+                   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+                 </Link >
   <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 
       Try AntHeatlh AI!
