5a7b0348f71deb21bd184be5374bbd87276076fe
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-18 22:47:12 -0400

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -284,9 +284,15 @@ const Navbar = () => {
                                         
                                         <Menu.Button className="flex rounded-full bg-sky-600 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-sky-600">
                                           <span className="sr-only">Open user menu</span>
-                                          <img
-                                           src="usericon.png" width={44} height={44} className="flex rounded-full"
-                                          />
+                                          
+                                          
+                                          
+                                             
+                                             
+<div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+    <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+</div>
+
                                         </Menu.Button>
                                       
                                       </div>
