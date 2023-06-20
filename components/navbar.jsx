c178a3db27cb2b85bda3e49634bb1b7565777262
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-20 17:44:18 -0400

    ..

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -324,12 +324,9 @@ const Navbar = () => {
                                           <Menu.Item>
                                             {({ active }) => ( 
                                             
-                                              <><LogAuth /><a
-                                                  href="#"
-                                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                >
-                    
-                                                </a></>
+                                            <a className={classNames(active ? 'bg-gray-100' : '', 'flex px-4 py-2 text-sm.text-gray-700')}>
+                                            <LogAuth />
+                                            </a>
                                             )}
                                           </Menu.Item>
                                           
