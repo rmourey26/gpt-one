859d5ebbde602c7530a95cc45d21b2e329be90c6
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-20 15:41:53 -0400

    .

--- pages/_document.js
+++ pages/_document.js
@@ -7,8 +7,9 @@ export default function Document() {
         <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=8a43adc3-5c36-4cb7-bc9b-bc7dfe86162d"> </script>  
         </Head>
       <body>
-    <script async src="https://cse.google.com/cse.js?cx=80972874a71de4ef6">
-</script>
+  
+    
+
 
 
         <Main />
