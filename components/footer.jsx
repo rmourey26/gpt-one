85d4b9b43655e7614245f08c8b6a1d97085db8f6
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-06-28 13:22:49 -0400

    ..

--- components/footer.jsx
+++ components/footer.jsx
@@ -239,7 +239,7 @@ const Footer = () => {
               />
 
               <button
-                class="block rounded-full bg-violet-500 px-8 py-3 font-medium text-white transition hover:bg-violet-900"
+                class="block text-white bg-gradient-to-r from-violet-500 to-sky-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-violet-300 dark:focus:ring-violet-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
                 type="submit"
               >
                 Subscribe
