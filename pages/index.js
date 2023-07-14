3cc3d62154c6c88b419b96c44bb67ec7824b00fe
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-14 15:58:51 -0400

    ..

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
               <p class="max-w-2xl text-center mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"></p>
               <div className="items-center px-4 flex justify-center" >
                   <div className="z-50 relative mr-3">
-                <Link className="z-50" href="/antchat" passHref>
+                <Link className="z-50" href="/rumi" passHref>
                     <button className="z-50 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-violet-500 to-blue-500 group-hover:from-violet-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-violet-200 dark:focus:ring-violet-800">
   <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
       Meet RUMI!
