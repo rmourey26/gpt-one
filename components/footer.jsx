4f936cbd6e9adf672f8f72173a29e1b78e564793
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-14 19:07:52 -0400

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -261,7 +261,7 @@ const Footer = () => {
             href="/"
             rel="noreferrer"
             target="_blank"
-            class="text-white bg-gradient-to-r from-violet-500 to-sky-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-violet-300 dark:focus:ring-violet-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+            class="text-white bg-gradient-to-r from-violet-500 to-sky-500 transition hover:bg-gradient-to-bl"
           >
             <span class="sr-only">Facebook</span>
             <svg
