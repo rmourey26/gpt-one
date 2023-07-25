fa79da3d07e02e5e6c5909d9056f1ce7c0f726dd
Robert Mourey Jr <rmourey_jr@quantumone.network>
2023-07-24 23:49:34 -0400

    Update registration.js

--- pages/registration.js
+++ pages/registration.js
@@ -77,9 +77,9 @@ export default function Example() {
 
 
 <Navbar />
-<div className="space-y-12 lg:m-36 md:m-20 sm:m-10">
+<div className="mt-10 md:w-96 md:max-w-full mx-auto">
     <form action="/api/form" method="post">
-      <div className="space-y-12 lg:m-36 md:m-20 sm:m-10">
+      <div className="mt-10 md:w-96 md:max-w-full mx-auto">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold text-center text-gray-900">Hearti Registration/ Sign Up Process</h2>
           <p className="mt-1 text-sm text-center text-gray-600">
