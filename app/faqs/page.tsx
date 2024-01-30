51cca319aa15fab3a8c494b1f27bbdddc62a595b
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-29 22:50:00 -0500

    Update page.tsx

---
+++ app/faqs/page.tsx
@@ -0,0 +1,19 @@
+import { useState, useRef, useEffect, Fragment } from 'react'
+import Accordion from '../../components/Accordion';
+import Content from '../../components/Content';
+
+
+
+const Faqs = () => {
+  
+Accordion({Content})
+
+return (
+    
+   
+      <><Accordion/></>
+      
+      );
+};
+
+export default Faqs
