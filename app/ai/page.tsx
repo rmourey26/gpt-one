55017e12d52df4742fe3dc5316a85c71ac3ef509
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-01-31 23:05:13 +0000

    .

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -80,7 +80,7 @@ export default function AI() {
     </div>
   </div>
   </div>
-  <form onSubmit={onSubmit}>
+  <form>
   <div className="mb-6">
   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
   <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
