08f022bdf5818ca0331ba6aa5048e4644d75e843
Robert Mourey Jr <rmourey_jr@quantumone.io>
2024-08-19 15:12:35 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    `
+
 const nextConfig = {
   reactStrictMode: true,
   images : {
     domains : ['quantumone.b-cdn.net', 'unpkg.com', 'lh3.googleusercontent.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   },
   
 };
